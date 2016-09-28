843c289becee5a527b32cfea7ab02c4a55bd0292
Weffy <k.pongsema@gmail.com>
2016-09-28 09:24:02 -0400

    need to check if data is being placed in pages...everything above that seems to be working

--- src/OS.java
+++ src/OS.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 public class OS {
 
 	static HashMap<Integer, Integer> pageTable = new HashMap<Integer, Integer>();
-	Page[] pageObj; //memory
+	static Page[] pageObj; //memory
 	String tableSizes[];
 	
 	
@@ -25,6 +25,7 @@ public class OS {
 		
 		try {
 			inputFile = new BufferedReader( new FileReader( filename ) );
+//			System.out.println("File exists...");
 		} catch (FileNotFoundException e1) {
 			System.out.println("File does not exist...");
 		}
@@ -36,6 +37,7 @@ public class OS {
 		String initialLine = null;
 		try {
 			initialLine = inputFile.readLine();
+			System.out.println(initialLine);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -45,11 +47,12 @@ public class OS {
 		tableSizes = initialLine.split(" ");
 		int numPages = Integer.parseInt( tableSizes[0] );
 		int numBytes = Integer.parseInt( tableSizes[1] );
-		
+		System.out.println("numPages: " + numPages);
+		System.out.println("numBytes: " + numBytes);
 		//set the size of the array
 		pageObj = new Page[numPages];
 		String reader = null;
-
+		System.out.println(pageObj.length);
 		//now that we set the size of the pageObj array
 		//read through file and...
 		//create pageTable mappings
@@ -60,11 +63,15 @@ public class OS {
 		
 		try {
 			while ( ( reader = inputFile.readLine() ) != null) {
+//				System.out.println("Enter while loop");
 				int counter = 0;
 				if (isItData(reader) ) {
 					//if true, then this is data...handle accordingly
-					String[] split = reader.split("\\-\\>");
+					System.out.println("if fired...");
+					String[] split = reader.split("\\->");
 					pageTable.put( Integer.parseInt( split[0] ),  Integer.parseInt( split[1] ) );
+					System.out.println(Integer.parseInt( split[0]));
+					System.out.println(Integer.parseInt( split[1]));
 				} else {
 					//if false, then not data, handle accordingly...
 					byte[] data = new byte[numBytes]; 
@@ -85,7 +92,8 @@ public class OS {
 	
 	private boolean isItData(String reader) {
 		//regex
-		String strSearch = "d+\\-\\>d+";
+		System.out.println("is it data?");
+		String strSearch = "d*\\->d*";
 		Pattern patSearch = Pattern.compile(strSearch);
 		Matcher matSearch = patSearch.matcher(reader);
 		return matSearch.find();
@@ -102,7 +110,8 @@ public class OS {
 	
 	public static void main(String[] args) {
 		
-		
+		OS os = new OS("/Users/Krirk-Mac/Documents/workspace/OSProj2/src/proj2_data1.txt");
+
 	}
 	
 }
