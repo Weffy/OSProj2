9791ee1dd995063ddf71ec1f9eefedd42c0a8388
Weffy <k.pongsema@gmail.com>
2016-09-29 16:33:10 -0400

    passed 3 tests...

--- src/OS.java
+++ src/OS.java
@@ -37,7 +37,7 @@ public class OS {
 		String initialLine = null;
 		try {
 			initialLine = inputFile.readLine();
-			System.out.println(initialLine);
+//			System.out.println(initialLine);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -47,12 +47,12 @@ public class OS {
 		tableSizes = initialLine.split(" ");
 		int numPages = Integer.parseInt( tableSizes[0] );
 		int numBytes = Integer.parseInt( tableSizes[1] );
-		System.out.println("numPages: " + numPages);
-		System.out.println("numBytes: " + numBytes);
+//		System.out.println("numPages: " + numPages);
+//		System.out.println("numBytes: " + numBytes);
 		//set the size of the array
 		pageObj = new Page[numPages];
 		String reader = null;
-		System.out.println(pageObj.length);
+//		System.out.println(pageObj.length);
 		//now that we set the size of the pageObj array
 		//read through file and...
 		//create pageTable mappings
@@ -67,11 +67,11 @@ public class OS {
 				int counter = 0;
 				if (isItAMap(reader) ) {
 					//if true, then this is data...handle accordingly
-					System.out.println("if fired...");
+//					System.out.println("if fired...");
 					String[] split = reader.split("\\->");
 					pageTable.put( Integer.parseInt( split[0] ),  Integer.parseInt( split[1] ) );
-					System.out.println(Integer.parseInt( split[0]));
-					System.out.println(Integer.parseInt( split[1]));
+//					System.out.println(Integer.parseInt( split[0]));
+//					System.out.println(Integer.parseInt( split[1]));
 				} else {
 					//if false, then not data, handle accordingly...
 					byte[] data = new byte[numBytes]; 
@@ -93,27 +93,28 @@ public class OS {
 	
 	private boolean isItAMap(String reader) {
 		//regex
-		System.out.println("is it data?");
+//		System.out.println("is it data?");
 		String strSearch = "d*\\->d*";
 		Pattern patSearch = Pattern.compile(strSearch);
 		Matcher matSearch = patSearch.matcher(reader);
 		return matSearch.find();
 	}
 
-	private static int getPPN( int vpn ) {
+	public int getPPN( int vpn ) {
 		return pageTable.get( vpn );
 	}
 	
-	private static byte getDataAtVirtAddress( int virtAddress ) {
+	public static byte getDataAtVirtAddress( int virtAddress ) {
 		//not sure how I am supposed to use this method...
 		return (Byte) null;
 	}
 	
 	public static void main(String[] args) {
 		
-		OS os = new OS("/Users/Krirk-Mac/Documents/workspace/OSProj2/src/proj2_data1.txt");
-		System.out.println(getPPN(2));
-
+		OS os1 = new OS("/Users/Krirk-Mac/Documents/workspace/OSProj2/src/proj2_data1.txt");
+		OS os2 = new OS("/Users/Krirk-Mac/Documents/workspace/OSProj2/src/proj2_data2.txt");
+		System.out.println("PPN " + os1.getPPN(2));
+		System.out.println("PPN " + os2.getPPN(2));
 	}
 	
 }
