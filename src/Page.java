291912584fd51d934e426cf1c61e60abf51723fd
Krirk Pongsema <Weffy@users.noreply.github.com>
2016-10-24 10:49:10 -0400

    Update Page.java

--- src/Page.java
+++ src/Page.java
@@ -1,48 +1,38 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 
 public class Page {
-	int pageNum;
+	int vpn; //virt page #
 	byte[] data;
 	
-	public Page(int counter, int numBytes, byte[] data) {
-		this.pageNum = counter;
-		this.data = new byte[ numBytes ];
-		this.data = data;
-	}
-
-	private byte getByte( int offset ) {
-		return this.data[offset];
-	}
-	
-	
-	public boolean exists( int offset ) {
-		if (this.data.length == 0 || offset < 0) {
-			return false;
-		} else {
-			return true;
-		}
+	public Page(int vpn, String line, int size) {
+		this.vpn = vpn;
+		this.data = new byte[ size ];
+//		System.out.println("line: " + line);
+		this.data = line.getBytes();
+//		System.out.println("size: " + data.length);
 	}
 	
 	//this method needs to throw an exception
 	//her test code is attempting a catch
 	//else it fails
-	public byte getData( int offset ) throws Exception {
-		
-		try {
-			return this.getByte( offset );	
-		} catch (Exception e1) {
-//			System.out.println("invalid offset");
-//			throw new Exception("transction: " + transNbr); 
-//			throw new Exception("transction: " + transNbr, E); 
-
-//			System.out.println("offset: " + offset + " throwing exception...");
-			throw new Exception("invalid offset");
-//			byte nullByte = (Byte) null;
-//			return nullByte;
-		}
-		
-
+	public byte getData( int offset ) throws ArrayIndexOutOfBoundsException {
+//		System.out.println("offset: " + offset);
+	
+	if (offset < 0 || offset > data.length) {
+		throw new ArrayIndexOutOfBoundsException("invalid offset");
+	} else {
+		return this.data[offset];
 	}
+			
+	}
+	public String returnData() {
+		String line = data.toString();
+		return line;
+	}
+	
+	public int dataSize() {
+		return data.length;
+	}
+	
+
 }
